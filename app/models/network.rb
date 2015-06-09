a23a31e5d71338663058457b4f4d27f50f31f813
robertpowers <robert.powers88@gmail.com>
2015-06-08 20:10:49 -0500

    Final Commit
    Fixed the bug

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,6 +1,6 @@
 class Network < ActiveRecord::Base
-  validates :theme_id, :presence => true
-  validates :network_id, :presence => true
+  validates :name, :presence => true
+  validates :location, :presence => true
 
   has_many :learningunits , :class_name => "Learningunit", :foreign_key => "network_id"
   has_many :schools , :class_name => "School", :foreign_key => "network_id"
